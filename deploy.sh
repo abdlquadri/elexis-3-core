a16c52023ab8b63af5eaf02f14b86fac014acac1
gerry <weirich@elexis.ch>
2016-05-30 15:44:03 +0200

    symlink to latest artifacts

--- deploy.sh
+++ deploy.sh
@@ -8,11 +8,11 @@ P2=${WEBSPACE}/p2/elexis-core
 mkdir -p ${PRODUCTS}/${BUILD_NUMBER}
 mkdir -p ${P2}/${BUILD_NUMBER}
 
-cp lucinda-server/target/lucinda-server*.jar ${PRODUCTS}/${BUILD_NUMBER}
-cp lucinda-client/target/lucinda-client*.jar ${PRODUCTS}/${BUILD_NUMBER}
+cp lucinda-server/target/products/*.zip ${PRODUCTS}/${BUILD_NUMBER}
 cp -R elexis-docmgr-lucinda-p2/target/repository/* ${P2}/${BUILD_NUMBER}
 
 rm ${PRODUCTS}/latest
 ln -s ${BUILD_NUMBER} ${WEBSPACE}/products/elexis-core/latest
 rm ${P2}/latest
 ln -s ${BUILD_NUMBER} ${WEBSPACE}/p2/elexis-core/latest
+
