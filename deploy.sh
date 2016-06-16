8924ebd08464b5886839a26c99c039c2e460fac3
gerry <weirich@elexis.ch>
2016-06-16 08:32:19 +0200

    use env

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-WEBSPACE=/var/www/vhosts/elexis.ch/httpdocs/ungrad
 
 PRODUCTS=${WEBSPACE}/products/elexis-core
 P2=${WEBSPACE}/p2/elexis-core
@@ -15,4 +14,3 @@ rm ${PRODUCTS}/latest
 ln -s ${BUILD_NUMBER} ${WEBSPACE}/products/elexis-core/latest
 rm ${P2}/latest
 ln -s ${BUILD_NUMBER} ${WEBSPACE}/p2/elexis-core/latest
-
